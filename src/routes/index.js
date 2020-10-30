9eaec34e5f69704cfe1d551751f7f57c16271af7
Raphael <raph080@hotmail.com>
2020-10-30 17:10:07 +0100

    test branching 001

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,12 @@ const formRouter = require("./form");
 const homepageRouter = require("./homepage");
 
 const routeName = (name) => (req, res, next) => {
-  res.locals.routename = name;
-  next();
+   res.locals.routename = name;
+   next();
 };
 
 router.use("/", routeName("home"), homepageRouter);
 router.use("/form", routeName("form"), formRouter);
+router.use("/about", routeName("about"), formRouter);
 
 module.exports = router;
