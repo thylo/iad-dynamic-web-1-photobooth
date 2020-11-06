cc03be4542d31432f25661bc834fe1cec08d219c
MatisR <matis.rondiat@etu.iad-arts.be>
2020-11-06 16:08:48 +0100

    Database resolve #9

--- src/routes/form.js
+++ src/routes/form.js
@@ -6,36 +6,58 @@ const moods = ["happy", "bored", "sad", "neutral"];
 
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
-  console.log(res.locals);
-  res.render("form/create.html", {
-    title: "Create new selfie",
-    moods,
-    errors: res.locals.errors || [],
-  });
+	console.log(res.locals);
+	res.render("form/create.html", {
+		title: "Create new selfie",
+		moods,
+		errors: res.locals.errors || [],
+	});
 });
 
 router.post("/", function (req, res, next) {
-  const body = req.body;
-  console.log(body);
-  let hasError = false;
-  if (!body.name) {
-    hasError = true;
-    req.flash("errors", { field: "name", msg: "Please select a name" });
-  }
-  if (!body.mood) {
-    hasError = true;
-    req.flash("errors", { field: "mood", msg: "Please select a mood" });
-  }
-  if (hasError) {
-    req.flash("messages", {
-      level: "error",
-      msg: "Ooops something went wrong..."
-    });
-    res.redirect("/form");
-    return;
-  }
-  req.flash("messages", { level: "success", msg: "Created" });
-  res.redirect("/");
+	const body = req.body;
+	console.log(body);
+	let hasError = false;
+	if (!body.name) {
+		hasError = true;
+		req.flash("errors", { field: "name", msg: "Please select a name" });
+	}
+	if (!body.mood) {
+		hasError = true;
+		req.flash("errors", { field: "mood", msg: "Please select a mood" });
+	}
+	if (hasError) {
+		req.flash("messages", {
+			level: "error",
+			msg: "Ooops something went wrong...",
+		});
+		res.redirect("/form");
+		return;
+	}
+
+	//insérer données reçues
+	db.insert(
+		{
+			time: Date.now(),
+			name: req.body.name,
+			mood: req.body.mood,
+		},
+		function (error, data) {
+			if (error) {
+				//si erreur
+				req.flash("messages", {
+					level: "error",
+					msg: "Error: could not save in database, try again",
+				});
+				res.redirect("/form");
+				return;
+			}
+			//si c'est fait, rediriger
+
+			req.flash("messages", { level: "success", msg: "Created" });
+			res.redirect("/");
+		}
+	);
 });
 
 module.exports = router;
