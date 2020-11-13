b308b8a44dfacdc80b6ea217015b368fcf48f88a
HugoTaburiaux <hugo.taburiaux@etu.iad-arts.be>
2020-11-13 15:43:49 +0100

    Link selfies db

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -6,9 +6,9 @@ router.get("/", async function (req, res, next) {
    res.render("index.html", {
       title: "Homepage",
       selfieData: [
-         ["raph", "https://picsum.photos/id/100/100/100", "#"],
-         ["robin", "https://picsum.photos/id/102/200/100", "#"],
-         ["hugo", "https://picsum.photos/id/104/100/200", "#"],
+         ["raph", "https://picsum.photos/id/100/100/100", "#1",],
+         ["robin", "https://picsum.photos/id/102/200/100", "#2",],
+         ["hugo", "https://picsum.photos/id/104/100/200", "#3",],
       ],
    });
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,11 @@ const formRouter = require("./form");
 const mapRouter = require("./map");
 const homepageRouter = require("./homepage");
 const aboutRouter = require("./about");
+const db = require("../config/database");
 
 const routeName = (name) => (req, res, next) => {
-   res.locals.routename = name;
-   next();
+  res.locals.routename = name;
+  next();
 };
 
 router.use("/", routeName("home"), homepageRouter);
@@ -16,5 +17,14 @@ router.use("/form", routeName("form"), formRouter);
 router.use("/map", routeName("map"), mapRouter);
 router.use("/contact", routeName("contact"), contactRouter);
 router.use("/about", routeName("about"), aboutRouter);
-
+router.use("/selfies/:selfieId", (req, res) => {
+  const id = req.params.selfieId;
+  db.find({ _id: id }, (error, doc) => {
+    if (error) {
+      res.send("il n y a pas de document");
+      return;
+    }
+    res.render("selfie.html", { doc });
+  });
+});
 module.exports = router;
