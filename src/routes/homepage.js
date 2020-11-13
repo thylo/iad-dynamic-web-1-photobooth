5e4b0db5b3b9e9eb33012814055843c90f1e02d0
MatisR <matis.rondiat@etu.iad-arts.be>
2020-11-13 14:09:13 +0100

    resolve #25 #28

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -5,6 +5,11 @@ const router = express.Router();
 router.get("/", async function (req, res, next) {
    res.render("index.html", {
       title: "Homepage",
+      selfieData: [
+         ["raph", "https://picsum.photos/id/100/100/100", "#"],
+         ["robin", "https://picsum.photos/id/102/200/100", "#"],
+         ["hugo", "https://picsum.photos/id/104/100/200", "#"],
+      ],
    });
 });
 
--- src/routes/map.js
+++ src/routes/map.js
@@ -1,35 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/database");
 
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
+   data = [];
+
+   db.find({}, function (err, docs) {
+      if (err == null) {
+         docs.forEach((entry) => {
+            console.log(entry);
+            curObj = {
+               name: entry["name"],
+               imageUrl: "https://picsum.photos/id/100/100/100",
+               lat: 50,
+               lon: 0,
+               emotion: entry["mood"],
+            };
+            data.push(curObj);
+            console.log(data);
+         });
+      }
+   });
+
+   console.log(
+      "string" +
+         JSON.stringify({
+            contacts: data,
+         })
+   );
+
    res.render("map.html", {
       title: "Show location of selfies",
       leafletRequired: "true",
       jsonObject: JSON.stringify({
-         contacts: [
-            {
-               name: "name1",
-               imageUrl: "https://picsum.photos/id/100/100/100",
-               lat: 51.5,
-               lon: -0.09,
-               emotion: "happy",
-            },
-            {
-               name: "name2",
-               imageUrl: "https://picsum.photos/id/200/100/100",
-               lat: 52.5,
-               lon: -5.09,
-               emotion: "sad",
-            },
-            {
-               name: "name3",
-               imageUrl: "https://picsum.photos/id/300/100/100",
-               lat: 40.5,
-               lon: 7.09,
-               emotion: "gorgous",
-            },
-         ],
+         contacts: data,
       }),
    });
 });
