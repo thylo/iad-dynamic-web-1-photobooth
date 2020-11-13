9a25c78f40c29cc298407ab209c6afa2bb398aa5
Raphael <raph080@hotmail.com>
2020-11-13 15:08:01 +0100

    replace dummy contacts to DB contacts

--- src/routes/map.js
+++ src/routes/map.js
@@ -9,33 +9,24 @@ router.get("/", async function (req, res, next) {
    db.find({}, function (err, docs) {
       if (err == null) {
          docs.forEach((entry) => {
-            console.log(entry);
             curObj = {
                name: entry["name"],
-               imageUrl: "https://picsum.photos/id/100/100/100",
+               imageUrl: entry["image"]["path"],
                lat: 50,
                lon: 0,
                emotion: entry["mood"],
             };
             data.push(curObj);
-            console.log(data);
+         });
+
+         res.render("map.html", {
+            title: "Show location of selfies",
+            leafletRequired: "true",
+            jsonObject: JSON.stringify({
+               contacts: data,
+            }),
          });
       }
-   });
-
-   console.log(
-      "string" +
-         JSON.stringify({
-            contacts: data,
-         })
-   );
-
-   res.render("map.html", {
-      title: "Show location of selfies",
-      leafletRequired: "true",
-      jsonObject: JSON.stringify({
-         contacts: data,
-      }),
    });
 });
 
